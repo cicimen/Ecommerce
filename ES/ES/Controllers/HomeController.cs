0502f4a1f58835487bb6b6ecdf32a9dda2933d03
cicimen <egemengulkilik@gmail.com>
2013-10-29 02:50:54 +0200

    ss

---
+++ ES/ES/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ES.Models;
+
+namespace ES.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ESDBDataContext Db = new ESDBDataContext();
+
+        public ActionResult Index()
+        {
+            return View(
+                        new { 
+                            CategoryList = Db.Categories.OrderBy(d=> d.OrderNumber).ToList()
+                        }
+                );
+        }
+
+    }
+}
